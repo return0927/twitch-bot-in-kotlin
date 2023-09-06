c1060eac0e883200fda68f98a1409afbe58a43ea
Eunhak Lee <lee@enak.kr>
2023-09-07 00:48:40 +0900

    feat: log detail steps of clip url detection

--- src/main/kotlin/kr/enak/luya/luyasupport/twitch/commands/CommandClipImpl.kt
+++ src/main/kotlin/kr/enak/luya/luyasupport/twitch/commands/CommandClipImpl.kt
@@ -3,6 +3,8 @@ package kr.enak.luya.luyasupport.twitch.commands
 import com.github.twitch4j.chat.events.channel.ChannelMessageEvent
 import kotlinx.coroutines.CompletableDeferred
 import kr.enak.luya.luyasupport.twitch.abc.IncomingCommandDto
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 class CommandClipImpl(
     name: String,
@@ -12,12 +14,19 @@ class CommandClipImpl(
     aliases = aliases
 ) {
     companion object {
+        private lateinit var logger: Logger
+
         private val REG = """@[^,]+, 클립이 생성되었습니다 -> (https://vod\.twip\.kr/clip/.+)""".toRegex()
 
         private val received = CompletableDeferred<String?>()
     }
 
+    init {
+        logger = LoggerFactory.getLogger(this::class.simpleName)
+    }
+
     override suspend fun execute(dto: IncomingCommandDto): String? {
+        logger.info("Await until clip url is detected for user ${dto.user.name}")
         val url = received.await() ?: return "클립 정보 수신에 실패했습니다."
 
         return super.execute(
@@ -30,6 +39,10 @@ class CommandClipImpl(
 
     object ClipURLMessageDetector {
         fun onChat(event: ChannelMessageEvent) {
+            if (event.user.id != "ssakdook") return
+            else if (event.message == "클립 생성을 요청했습니다.") return
+            logger.info("Detected message candidate of clip url: ${event.message}")
+
             val match = REG.find(event.message)
             val url: String? = match?.groupValues?.get(1)
             received.complete(url)
